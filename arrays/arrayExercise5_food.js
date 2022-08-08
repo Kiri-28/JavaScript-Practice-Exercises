6486db9b0302ba3c348b868de6bc89b0e1da6f2e
Kiri-28 <lauratxiki28@gmail.com>
2022-08-08 14:34:20 +0200

    Changed a couple of comments in arrayExercise5

--- arrays/arrayExercise5_food.js
+++ arrays/arrayExercise5_food.js
@@ -2,11 +2,12 @@
 let food1 = ['Noodle', 'Pasta', 'Ice-cream'];
 let food2 = ['Fries', 'Ice-cream', 'Pizza'];
 
-//Create a function that console.logs the repeated items
+// Create a function that console.logs the repeated items
 function showCommonFood(array1, array2) {
     // Iterate over the first array and for each element iterate over the second array searching for a coincidence
     for(let array1Index = 0; array1Index < array1.length; array1Index++) {
         for(let array2Index = 0; array2Index < array2.length; array2Index++) {
+            // Write an if that checks if there are coincident items, and if there are, print that item
             if(array1[array1Index] === array2[array2Index]) {
                 console.log(array1[array1Index]);
             }   
