1f5535760f1db16275ccf09505fd3b82aca71efe
Kiri-28 <lauratxiki28@gmail.com>
2022-08-08 14:22:00 +0200

    Added a solved 5th exercise

---
+++ arrays/arrayExercise5_food.js
@@ -0,0 +1,17 @@
+// Compare the two arrays and find common food if any
+let food1 = ['Noodle', 'Pasta', 'Ice-cream'];
+let food2 = ['Fries', 'Ice-cream', 'Pizza'];
+
+//Create a function that console.logs the repeated items
+function showCommonFood(array1, array2) {
+    // Iterate over the first array and for each element iterate over the second array searching for a coincidence
+    for(let array1Index = 0; array1Index < array1.length; array1Index++) {
+        for(let array2Index = 0; array2Index < array2.length; array2Index++) {
+            if(array1[array1Index] === array2[array2Index]) {
+                console.log(array1[array1Index]);
+            }   
+        }
+    } 
+}
+
+showCommonFood(food1, food2);
