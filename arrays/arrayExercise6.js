255cbcfc71f4ef7f27311b8100f88177723a2336
Kiri-28 <lauratxiki28@gmail.com>
2022-08-08 14:59:52 +0200

    Added an unsolved arrayExercise6

---
+++ arrays/arrayExercise6.js
@@ -0,0 +1,16 @@
+// Compare the 3 arrays and find common elements
+let values1= ['Apple', 1, false];
+let values2 = ['Fries', 2 ,true];
+let values3 = ['Mars', 9, 'Apple'];
+
+// Create a function that prints the repeated items
+function showCommonItems(array1, array2, array3) {
+    // Iterate over the first array, and for each item check if its repeated in the other two
+    for(let array1Index = 0; array1Index < array1.length; array1Index++) {
+        for(let arrayIndex = 0; arrayIndex < array2.length; arrayIndex++) {
+            
+        }
+    }
+}
+
+
