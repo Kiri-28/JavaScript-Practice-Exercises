48ca8434e1688d2b3334f88334c4956e825ced21
Aingeru <aingerusanchez72@gmail.com>
2022-08-09 14:39:23 +0200

    chore(arrayExercise7): comment full word console log

--- arrays/arrayExercise7.js
+++ arrays/arrayExercise7.js
@@ -1,15 +1,16 @@
 // For each item in this array console.log the letters in each item
-let furniture = ['Table', 'Chairs','Couch'];
+let furniture = ['Table', 'Chairs', 'Couch'];
 
 // Create a function that prints the letters of every item
 function printLetters(array) {
     // Write a for loop that iterates through the array
-    for(let arrayIndex = 0; arrayIndex < array.length; arrayIndex++) {
+    for (let arrayIndex = 0; arrayIndex < array.length; arrayIndex++) {
         const word = array[arrayIndex];
         // Log the word you want to spell later
-        console.log(word);
+        // console.log(word);
+
         // Write a for loop that iterates through each word
-        for(let letterIndex = 0; letterIndex < word.length; letterIndex++) {
+        for (let letterIndex = 0; letterIndex < word.length; letterIndex++) {
             // Print each letter
             console.log(word[letterIndex]);
         }
