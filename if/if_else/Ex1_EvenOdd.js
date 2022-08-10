f022cf885f343f0c4c2239af73f9d6039fa01237
Aingeru <aingerusanchez72@gmail.com>
2022-08-10 15:09:22 +0200

    feat(if): aswer of "even or odd"

--- if/if_else/Ex1_EvenOdd.js
+++ if/if_else/Ex1_EvenOdd.js
@@ -1,2 +1,16 @@
-/* Write a "isEvenOrOdd" function that checks if the number is even or odd by using "%".
-Print "Number is even" if its divisible by 2 or "Number is odd" if it isn't. */
+/**
+ * Write a "isEvenOrOdd" function that checks if the number is even or odd by using "%".
+ * Print "Number is even" if its divisible by 2 or "Number is odd" if it isn't.
+ */
+function isEvenOrOdd(number) {
+    // Check it argument it's a number
+    if (typeof +number !== 'number') throw new Error("Argument data it's not of correct type. Should be a number");
+
+    // Check if number is even or odd and pupulate reuslt message to log and return
+    const result = (number % 2 === 0) ? 'Number is even' : 'Number is odd';
+    console.log(result);
+    return result;
+}
+
+const number = 4;
+isEvenOrOdd(number);
