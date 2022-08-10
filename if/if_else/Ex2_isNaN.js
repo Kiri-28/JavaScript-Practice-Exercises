eca4837cfb8b5881942594fafb99f9c5e191e70c
Kiri-28 <lauratxiki28@gmail.com>
2022-08-10 21:07:14 +0200

    Solved Ex2_isNaN

--- if/if_else/Ex2_isNaN.js
+++ if/if_else/Ex2_isNaN.js
@@ -1,3 +1,19 @@
 /* Function `isNumber()` checks if input variable is a number by using isNaN() in-built JavaScript function.
 1. Print "Variable is not a number" if isNaN() returns true.
 2. Else print "Variable is a number" */
+
+// Declare a "input" variable in which we define the items we want to evaluate
+let input = 2;
+
+// Create a function that checks if "input" is a numer or not
+function isValidNumber(input) {
+    //No lets do an if: if the isNaN() function returns true, then console.log "is not a number"
+    if (isNaN(input)) {
+        console.log(input + " is not a number");
+    } else {
+        console.log(input + " is a number");
+    }
+}
+
+input = "lol";
+isValidNumber("lol");
