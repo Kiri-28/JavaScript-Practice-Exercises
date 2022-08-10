01d020bb47b07187fe3f51c3f8ffc349f0271ed3
Kiri-28 <lauratxiki28@gmail.com>
2022-08-10 14:26:36 +0200

    Added two new exercises to program_Structure

---
+++ program_structure/exercise2_fizz_buzz.js
@@ -0,0 +1,5 @@
+/*   Write a program that uses console.log to print the numbers from 1 to 100. For numbers divisible by 3, 
+  #  print "fizz" instead of the number; and for numbers divisible by 5 print "buzz" instead.
+     The next step is to modify the progra so it prints "FizzBuzz" in th place of nubers divisible by both 3 and 5 */
+
+     
