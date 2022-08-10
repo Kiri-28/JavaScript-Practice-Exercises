6f21d89c5cfae648ba4eda6e928049f41e1468c5
Aingeru <aingerusanchez72@gmail.com>
2022-08-10 15:21:10 +0200

    feat(if): answer of "isNaN"

--- if/if_else/Ex2_isNaN.js
+++ if/if_else/Ex2_isNaN.js
@@ -1,3 +1,13 @@
-/* Function `isNumber()` checks if input variable is a number by using isNaN() in-built JavaScript function.
-1. Print "Variable is not a number" if isNaN() returns true.
-2. Else print "Variable is a number" */
+/**
+ * Function `isNumber()` checks if input variable is a number by using isNaN() in-built JavaScript function.
+ * 1. Print "Variable is not a number" if isNaN() returns true.
+ * 2. Else print "Variable is a number
+ */
+function isNumber(value) {
+    const result = isNaN(value) ? 'Variable is not a number' : 'Variable is a number';
+    console.log(result);
+    return result;
+}
+
+const testValue = null;
+isNumber(testValue);
