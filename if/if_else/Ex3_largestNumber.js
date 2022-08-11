8d92704ee0a3afcb410aefd8224cf12f5e97d8b6
Kiri-28 <lauratxiki28@gmail.com>
2022-08-11 13:47:11 +0200

    Added solutions to Ex3 and Ex4 (if/if_else)

--- if/if_else/Ex3_largestNumber.js
+++ if/if_else/Ex3_largestNumber.js
@@ -1,4 +1,23 @@
-/* Function `findLargest()` finds the largest between two number by using “>” and “=” operator in JavaScript.
+/* Function `findLargest()` finds the largest between two numbers by using “>” and “=” operator in JavaScript.
     1. Print num1 is the largest if num1>num2.
     2. Print num2 is the largest if num1<num2.
     3. Else print num1 and num2 are equal when num1==num2. */
+
+//Write the function that evaluates the numbers
+function findLargestOfTwo(num1, num2) {
+    // Validate the parameters through an if and isNaN function
+    if(isNaN(num1) || isNaN(num2)) {
+        console.log("One of the parameters is not a number.");
+    }
+
+    // Number evaluation through ifs and elses
+    if(num1 > num2) {
+        console.log(num1 + " is the largest number.");
+    } else if(num1 < num2) {
+        console.log(num2 + " is the largest number.");
+    } else {
+        console.log(num1 + " and " + num2 + " are equal numbers.");
+    }
+}
+
+findLargestOfTwo(undefined, null);
