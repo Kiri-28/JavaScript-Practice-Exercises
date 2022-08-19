6cdd38063049ceef79f9ef292f0c96796d021193
Kiri-28 <lauratxiki28@gmail.com>
2022-08-19 14:51:00 +0200

    Added thee solution to exercise6 and exercise7

--- if/if_else/Ex7_operations.js
+++ if/if_else/Ex7_operations.js
@@ -6,3 +6,21 @@
    4. Print result of num1/num2 if operation is “divide”
    5. Print result of num1%num2 if operation is “modulus”
    6. Else print “Invalid operation” */
+
+function evalNumbers(num1, num2, operation) {
+  if (operation === 'add') {
+    console.log(`${num1} + ${num2} = ${num1 + num2}`);
+  } else if (operation === 'subtract') {
+    console.log(`${num1} - ${num2} = ${num1 - num2}`);
+  } else if (operation === 'multiply') {
+    console.log(`${num1} * ${num2} = ${num1 * num2}`);
+  } else if (operation === 'divide') {
+    console.log(`${num1} / ${num2} = ${num1 / num2}`);
+  } else if (operation === 'modulus') {
+    console.log(`${num1} % ${num2} = ${num1 % num2}`);
+  } else {
+    console.log('Invalid operation');
+  }
+}
+
+evalNumbers(4, 2, 'modulus');
