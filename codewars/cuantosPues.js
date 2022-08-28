ba22d04ffb596588f52b38af77a841244d1032ff
Aingeru <aingerusanchez72@gmail.com>
2022-08-29 00:30:49 +0200

    ejercicio de cuantasCoincidencias resuelto

--- codewars/cuantosPues.js
+++ codewars/cuantosPues.js
@@ -5,5 +5,21 @@
  * @returns {number} `number` devuelve el número de coincidencias del elemento en la lista
  */
 function cuantasCoincidencias(lista, elemento) {
+  // Declarar contador de coincidencias resultante
+  let coincidencias = 0;
 
+  // Recorrer la lista
+  for (let index = 0; index < lista.length; index++) {
+    const element = lista[index];
+    if (element === elemento) {
+      coincidencias++;
+    }
+  }
+
+  return coincidencias;
 }
+
+const listaTest = ['lechuga', 'tomate', 'queso', 'tomate', 'pavo', 'queso', 'maiz', 'tomate', 'pollo'];
+const elementoTest = 'tomate';
+const coincidencias = cuantasCoincidencias(listaTest, elementoTest);
+console.log(`El elemento "${elementoTest}" se repite ${coincidencias} veces en la lista.`);
