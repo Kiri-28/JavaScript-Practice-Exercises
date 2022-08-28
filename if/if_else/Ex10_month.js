46e2ca0496c15a47f800e182ccf446712a34dab0
Kiri-28 <lauratxiki28@gmail.com>
2022-08-29 00:13:38 +0200

    Solved ex9_grades and ex10_months

--- if/if_else/Ex10_month.js
+++ if/if_else/Ex10_month.js
@@ -4,3 +4,25 @@
    2. If month is equal to 2 ie, February print “29 days” if leap year else print “28 days” .
    3. Else if month is equal to 4, 6, 9 or 11 print “30 days”.
    4. Else print “31 days”. */
+
+function isLeapYear(year) {
+  return (((year % 4 === 0) && (year % 100 === 0)) || (year % 400 === 0));
+}
+
+function findDaysInMonth(month, year) {
+  if (month < 1 || month > 12) {
+    console.log('Invalid month');
+  } else if (month === 2) {
+    if (isLeapYear(year)) {
+      console.log('The month has 29 days');
+    } else {
+      console.log('The month has 28 days');
+    }
+  } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+    console.log('The month has 30 days');
+  } else {
+    console.log('The month has 31 days');
+  }
+}
+
+findDaysInMonth(2, 1996);
--- if/if_else/Ex9_grades.js
+++ if/if_else/Ex9_grades.js
@@ -8,3 +8,25 @@
    6. Print “E grade” if marks is between 40 and 50.
    7. Print “Student has failed” if marks is between 0 and 40.
    8. Else print “Invalid marks”. */
+
+function findGrade(mark) {
+  if (mark >= 90 && mark <= 100) {
+    console.log('S grade');
+  } else if (mark >= 80 && mark < 90) {
+    console.log('A grade');
+  } else if (mark >= 70 && mark < 80) {
+    console.log('B grade');
+  } else if (mark >= 60 && mark < 70) {
+    console.log('C grade');
+  } else if (mark >= 50 && mark < 60) {
+    console.log('D grade');
+  } else if (mark >= 40 && mark < 50) {
+    console.log('E grade');
+  } else if (mark >= 0 && mark < 40) {
+    console.log('Student has failed');
+  } else {
+    console.log('Invalid marks');
+  }
+}
+
+findGrade(100);
