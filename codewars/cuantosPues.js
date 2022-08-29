71dfe47f8e37705da3196b220236b2103edff9d2
Aingeru <aingerusanchez72@gmail.com>
2022-08-29 15:21:50 +0200

    coincidences: added tip comments

--- codewars/cuantosPues.js
+++ codewars/cuantosPues.js
@@ -4,17 +4,37 @@
  * @param {string} elemento de búsqueda
  * @returns {number} `number` devuelve el número de coincidencias del elemento en la lista
  */
-const listaTest = ['lechuga', 'tomate', 'queso', 'tomate', 'pavo', 'queso', 'maiz', 'tomate', 'pollo', 'tomate'];
-const elementoTest = 'tomate';
-
 function cuantasCoincidencias(lista, elemento) {
-  for (let index = 0; index < lista.length; index++) {
-    const element = lista[index];
-    if (element === elementoTest) {
-      return (coincidencias);
+    // 1. Crear un contador de coincidencias
+
+    // 2. Recorrer la lista en buscar de ese elemento
+    // e incrementar el contador de coincidencias cada vez que lo encuentras
+    for (let index = 0; index < lista.length; index++) {
+        const element = lista[index];
+        if (element === elemento) {
+            return (coincidencias);
+        }
     }
-  }
+
+    // 3. Devolver el contador de coincidencias
 }
 
-const coincidencias = cuantasCoincidencias(listaTest, elementoTest);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ZONA DE PRUEBAS, NO MODIFICAR NADA A PARTIR DE AQUÍ
+const listaDeLaCompra = ['lechuga', 'tomate', 'queso', 'tomate', 'pavo', 'queso', 'maiz', 'tomate', 'pollo', 'tomate'];
+const fruta = 'tomate';
+const coincidencias = cuantasCoincidencias(listaDeLaCompra, fruta);
 console.log(`El elemento "${elementoTest}" se repite ${coincidencias} veces en la lista.`);
