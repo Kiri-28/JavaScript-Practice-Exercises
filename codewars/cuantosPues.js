206b8fc4af59fb60214a3ad3a6a555f61070a735
Kiri-28 <lauratxiki28@gmail.com>
2022-08-29 15:14:02 +0200

    WIP coincidencias problem

--- codewars/cuantosPues.js
+++ codewars/cuantosPues.js
@@ -4,11 +4,17 @@
  * @param {string} elemento de búsqueda
  * @returns {number} `number` devuelve el número de coincidencias del elemento en la lista
  */
-function cuantasCoincidencias(lista, elemento) {
+const listaTest = ['lechuga', 'tomate', 'queso', 'tomate', 'pavo', 'queso', 'maiz', 'tomate', 'pollo', 'tomate'];
+const elementoTest = 'tomate';
 
+function cuantasCoincidencias(lista, elemento) {
+  for (let index = 0; index < lista.length; index++) {
+    const element = lista[index];
+    if (element === elementoTest) {
+      return (coincidencias);
+    }
+  }
 }
 
-const listaTest = ['lechuga', 'tomate', 'queso', 'tomate', 'pavo', 'queso', 'maiz', 'tomate', 'pollo'];
-const elementoTest = 'tomate';
 const coincidencias = cuantasCoincidencias(listaTest, elementoTest);
 console.log(`El elemento "${elementoTest}" se repite ${coincidencias} veces en la lista.`);
